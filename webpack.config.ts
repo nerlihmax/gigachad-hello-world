8463ec8edf037d02ba4b3da978b45eb8d960f6d6
Nerlih Max <nerlihmax@yandex.ru>
2021-11-01 03:19:09 +0700

    fix: у продакшн теперь первый приоритет

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
-  mode: process.env.PRODUCTION || 'development',
+  mode: 'production' || process.env.DEV,
   module: {
     rules: [
       {
